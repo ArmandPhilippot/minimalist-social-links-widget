80d4e2b3dd1b49a6dedb02ab808b2674eb752110
Armand Philippot <git@armandphilippot.com>
2021-04-21 14:41:30 +0200

    chore: create the public view of the widget

--- public/partials/minimalist-social-links-widget-public-display.php
+++ public/partials/minimalist-social-links-widget-public-display.php
@@ -14,15 +14,50 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-$mslwidget_default_title = __( 'Minimalist Social Links', 'msl-widget' );
-$mslwidget_title         = ! empty( $instance['title'] ) ? $instance['title'] : $mslwidget_default_title;
-$mslwidget_title         = apply_filters( 'widget_title', $mslwidget_title, $instance, $this->id_base );
+$mslwidget_title = ! empty( $instance['title'] ) ? $instance['title'] : '';
+$mslwidget_title = apply_filters( 'widget_title', $mslwidget_title, $instance, $this->id_base );
+
+$mslwidget_selected_social_profiles = ! empty( $instance['selected_social_profiles'] ) ? $instance['selected_social_profiles'] : array();
+$mslwidget_social_profiles          = $this->mslwidget_get_social_profiles();
 
 echo wp_kses_post( $args['before_widget'] );
 if ( ! empty( $mslwidget_title ) ) {
 	echo wp_kses_post( $args['before_title'] ) . esc_html( $mslwidget_title ) . wp_kses_post( $args['after_title'] );
 }
 ?>
-<!-- Your widget content here. -->
+<ul class="mslwidget__list">
+	<?php
+	foreach ( $mslwidget_social_profiles as $mslwidget_social_profile ) {
+		if ( array_key_exists( $mslwidget_social_profile->id, $mslwidget_selected_social_profiles ) && $mslwidget_selected_social_profiles[ $mslwidget_social_profile->id ] ) {
+			$mslwidget_username = $instance[ $mslwidget_social_profile->id ] ? $instance[ $mslwidget_social_profile->id ] : '';
+
+			if ( 'instance' === $mslwidget_social_profile->url ) {
+				$mslwidget_instance_name = substr( strrchr( $mslwidget_username, '@' ), 1 );
+				$mslwidget_username      = substr( $mslwidget_username, 0, strrpos( $mslwidget_username, '@' ) );
+
+				if ( '' !== $mslwidget_instance_name && '' !== $mslwidget_username ) {
+					$mslwidget_profile_link = 'https://' . trailingslashit( $mslwidget_instance_name ) . $mslwidget_username;
+				} else {
+					$mslwidget_profile_link = '';
+				}
+			} elseif ( 'feed' === $mslwidget_social_profile->url ) {
+				$mslwidget_profile_link = $mslwidget_username;
+			} else {
+				$mslwidget_profile_link = trailingslashit( $mslwidget_social_profile->url ) . $mslwidget_username;
+			}
+			?>
+			<li class="mslwidget__item">
+				<a href="<?php echo esc_url( $mslwidget_profile_link ); ?>" class="mslwidget__link">
+					<span class="mslwidget__profile-logo <?php echo esc_attr( 'mslwidget__profile-logo--' . $mslwidget_social_profile->id ); ?>"></span>
+					<span class="mslwidget__profile-name screen-reader-text">
+						<?php echo esc_html( $mslwidget_social_profile->name ); ?>
+					</span>
+				</a>
+			</li>
+			<?php
+		}
+	}
+	?>
+</ul>
 <?php
 echo wp_kses_post( $args['after_widget'] );
